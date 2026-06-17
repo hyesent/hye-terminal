e32d48c9802df231ad606215c5ddc92707763b7f
hyesent <hyacinthmichael36@gmail.com>
2026-06-17 12:24:36 +0100

    add PWA config for install app

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: false, // you already have public/manifest.json
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      devOptions: {
+        enabled: true // lets you test PWA in dev mode
+      }
+    })
+  ],
   server: {
     headers: {
       'Cross-Origin-Embedder-Policy': 'require-corp',
